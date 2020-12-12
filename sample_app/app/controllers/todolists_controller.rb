c0fe007094f605c63bd1bcbffef5a768b46c3c7f
DaichiIshikawa <sonar.pocket.0926@gmail.com>
2020-12-12 06:56:53 +0000

    [Add] 3-5

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -2,26 +2,26 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to todolists_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
-    @list = Listfind(params[:id])
+    @list = List.find(params[:id])
   end
-  
+
   private
 
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
+
 
 end
