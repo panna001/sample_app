cc3609153367af555160b394f01e530b542de867
DaichiIshikawa <sonar.pocket.0926@gmail.com>
2020-12-16 04:15:59 +0000

    [Finish] 3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '33a80a49c6106c822b0357cfe6a95bbc58a03e54adcfe95524bfd1cd5d4f2ec805c60fa7a34430c83916d3622911919539bf097c90284d291306845fcc8f72d5'
